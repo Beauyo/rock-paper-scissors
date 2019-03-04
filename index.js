9f306875ee93ce0edcd8af7dc9ddd491912fb7b3
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-03-04 16:40:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var Rock = document.getElementById("Rock")
+var Paper = document.getElementById("Paper")
+var Scissors = document.getElementById("Scissors")
+var btn = document.getElementById("btn");
+ 
+
+document.body.addEventListener("click", function(){
+
+}
